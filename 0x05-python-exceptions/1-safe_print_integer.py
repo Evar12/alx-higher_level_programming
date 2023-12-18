91b14156096244950b87952df2e9d6cdca3e2286
Evar12 <Evaristus12@gmail.com>
2023-12-18 13:37:00 -0800

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,18 +10,3 @@ def safe_print_integer(value):
     except TypeError:
         return False
 
-if __name__ == '__main__':
-    value = 89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = -89
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-    value = "Holberton"
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
