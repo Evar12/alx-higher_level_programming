d30409cddb699945f1b9f39127d7ac7e6312b168
Evar12 <Evaristus12@gmail.com>
2023-12-18 13:51:40 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
     count = 0
@@ -11,20 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
             pass
         else:
             count += 1
-
     print()
-    return count
-
-if __name__ == '__main__':
-    my_list = [1, 2, 3, 4, 5]
-
-    nb_print = safe_print_list_integers(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
-
+    return (count)
