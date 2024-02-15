1be8663377d1c77f465074df92752a143718bd55
Evar12 <Evaristus12@gmail.com>
2024-02-15 13:52:13 -0800

    more Sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
--- Lists all privileges for user_0d_1
--- Lists all privileges for user_0d_2
---SHOW GRANTS FOR 'user_0d_1'@'localhost';
---SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server.
 
 SHOW GRANTS FOR user_0d_1@localhost;
