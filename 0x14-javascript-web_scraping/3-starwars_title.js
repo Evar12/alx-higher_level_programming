247e4584b0a4c027ace65612ab81bf86fffc5f3e
Evar12 <Evaristus12@gmail.com>
2024-04-24 16:02:58 +0100

    web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-// Star Wars
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const content = JSON.parse(body);
-    console.log(content.title);
+    const data = JSON.parse(body);
+    console.log(data.title);
   }
-});	
+});
